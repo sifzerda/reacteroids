cb034a3a732fc0d33e70d852d68dfcfe103131a4
Troygit config --global user.email <tydamon@hotmail.com>
2026-05-20 16:32:09 +1000

    formatting

--- src/ecs/queries.js
+++ src/ecs/queries.js
@@ -3,15 +3,6 @@
 import { world } from './world';
 
 export const ships = world.with('ship');
-
 export const bullets = world.with('bullet');
-
-export const movable = world.with(
-  'x',
-  'y',
-  'vx',
-  'vy'
-);
-
-export const asteroids =
-  world.with('asteroid');
+export const movable = world.with( 'x', 'y', 'vx', 'vy' );
+export const asteroids = world.with('asteroid');
