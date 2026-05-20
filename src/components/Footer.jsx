5308aedf8767f6b890b970949f72b490de2f4d80
Troygit config --global user.email <tydamon@hotmail.com>
2026-05-20 13:51:17 +1000

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,23 +6,31 @@ export default function Footer() {
     return (
         <footer className="relative w-full bg-black text-white border-t border-cyan-500/60 dual-border">
 
-            <div className="flex flex-col items-center py-3">
+            <div className="py-1 text-center border-b border-cyan-500/60 bg-black/65 p-6 shadow-[0_0_24px_rgba(57,255,20,0.2)] backdrop-blur-sm">
 
-                <p className="text-xs text-cyan-500 uppercase font-audiowide flex items-center gap-2">
-                    <span>sifzerda</span>
+                {/* scanline overlay */}
+                <div className="scanlines" />
 
+                <h1 className="flex items-center justify-center gap-2 text-xs font-audiowide text-green-300 drop-shadow-[0_0_6px_rgba(0,255,0,0.9)]">
+                    <span> DAMON</span>
                     <a
-                        href="https://github.com/sifzerda/nx-asteroids"
+                        href="https://github.com/sifzerda/reacteroids"
                         target="_blank"
                         rel="noopener noreferrer"
-                        className="flex items-center gap-1 text-gray-400 hover:text-cyan-300 transition">
+                        className="flex items-center text-green-300 hover:text-cyan-300 transition">
                         <FaGithub className="text-base" />
                     </a>
-
-                    <span>2026</span>
-                </p>
+                   <span>2026</span>
+                </h1>
 
             </div>
+
+
         </footer>
     );
 }
+
+
+
+
+
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,12 +2,12 @@
 
 export default function Header() {
   return (
-    <div className="py-1 text-center border-b border-cyan-500/60 bg-black/65 p-6 shadow-[0_0_24px_rgba(57,255,20,0.2)] backdrop-blur-sm ">
+    <div className="py-1 text-center border-b border-cyan-500/60 bg-black/65 p-6 shadow-[0_0_24px_rgba(57,255,20,0.2)] backdrop-blur-sm">
 
       {/* scanline overlay */}
       <div className="scanlines" />
 
-      <h1 className="text-md font-audiowide tracking-[0.25em] text-green-300 drop-shadow-[0_0_6px_rgba(0,255,0,0.9)]">
+      <h1 className="text-xl font-audiowide text-green-300 drop-shadow-[0_0_6px_rgba(0,255,0,0.9)]">
         ASTEROIDS
       </h1>
 
