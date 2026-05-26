7cb7b97a17f9603c4b820e349cea1aa2d7cf0e88
Troygit config --global user.email <tydamon@hotmail.com>
2026-05-26 15:38:12 +1000

    changed exhaust to blue

--- src/ecs/factories/spawnExhaustParticle.js
+++ src/ecs/factories/spawnExhaustParticle.js
@@ -3,26 +3,29 @@
 import { world } from '../world';
 
 export function spawnExhaustParticle({
+  x,
+  y,
+  vx,
+  vy,
+}) {
+
+  world.add({
+
+    exhaust: true,
+
     x,
     y,
+
     vx,
     vy,
 
-}) {
+    life: 1.1,
 
-    world.add({
-        exhaust: true,
+    radius: 0.12,
 
-        x,
-        y,
-        vx,
-        vy,
-
-    life: 0.90,
-    radius: 0.04,
-    colorR: 1,
-    colorG: 0.4,
-    colorB: 0,
-
-    });
+    // hot blue core
+    colorR: 0.2,
+    colorG: 0.7,
+    colorB: 2.0,
+  });
 }
